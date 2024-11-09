bc8c5b0f62507f0728bc3615ca578cab2e5b504d
Szymon Kaput <szymonkaput123@gmail.com>
2024-11-09 13:15:43 +0100

    Update project with latest changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+const nextConfig = {
+  reactStrictMode: true,
+  output: "standalone", // Jeśli używasz aplikacji jako PWA, upewnij się, że to ustawienie jest prawidłowe.
+};
 
 export default nextConfig;
